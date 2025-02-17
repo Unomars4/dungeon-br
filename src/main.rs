e649b9bcfddf9355420f97a9eff625957b37f707
Une Nyide <une.nyide@gmail.com>
2025-02-17 12:47:13 +0200

    adjust screen height

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod player;
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
     pub use crate::player::*;
 }
