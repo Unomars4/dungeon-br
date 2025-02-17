e1e0104a57fb23ea4c68f634de117efd9f8eac09
Une Nyide <une.nyide@gmail.com>
2025-02-17 10:36:12 +0200

    add num_tiles constants in map module

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,9 @@
 //This file contains all the logic for the map functionality.
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
 pub fn hello_map() {
     println!("Hello map 🗺️");
+    println!("This is the total number of tiles needed: {} 🚀", NUM_TILES);
 }
