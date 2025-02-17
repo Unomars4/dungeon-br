b7e301e4a0136cde320bae918f40d934a1f9340a
Une Nyide <une.nyide@gmail.com>
2025-02-17 10:49:56 +0200

    add map constructor to map struct in map module

--- src/map.rs
+++ src/map.rs
@@ -3,14 +3,22 @@ use crate::prelude::*;
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
-pub struct Map {
-    pub tiles: Vec<TileType>,
-}
-
 #[derive(Clone, Copy, PartialEq)]
 pub enum TileType {
     Wall,
     Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
 }
 
 pub fn hello_map() {
