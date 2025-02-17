ec008e9c631e93522d99426f0fd846c855023c99
Une Nyide <une.nyide@gmail.com>
2025-02-17 10:31:31 +0200

    create personal prelude module to make access easier

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod map;
 
-use map::hello_map;
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 80;
+    pub use crate::map::*;
+}
+
+use prelude::*;
 
 fn main() {
     println!("Let's begin...😛");
