e6d1565c85567cd3a4707ee728d0bb1c3405b821
Une Nyide <une.nyide@gmail.com>
2025-02-19 11:39:32 +0200

    add map build struct, properties and implementation method fill

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -4,3 +4,15 @@ use std::usize;
 use crate::prelude::*;
 
 const NUM_ROOMS: usize = 20;
+
+pub struct MapBuilder {
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub player_start: Point,
+}
+
+impl MapBuilder {
+    fn fill(&mut self, tile: TileType) {
+        self.map.tiles.iter_mut().for_each(|tile| *t = tile);
+    }
+}
