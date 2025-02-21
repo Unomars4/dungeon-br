a93585cec03d816a707ba09eed593b3dbbb6e56a
Une Nyide <une.nyide@gmail.com>
2025-02-21 12:13:04 +0200

    fix pathname for resource path

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ fn main() -> BError {
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .with_tile_dimensions(32, 32)
-        .with_resource_path('/')
+        .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
