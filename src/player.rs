12005a8ac09b5249d0cf044f49767a10fc9942c2
Une Nyide <une.nyide@gmail.com>
2025-02-21 12:49:38 +0200

    connect player to camera, render player on second layer

--- src/player.rs
+++ src/player.rs
@@ -9,7 +9,7 @@ impl Player {
         Self { pos: point }
     }
 
-    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map, camera: &mut Camera) {
         if let Some(key) = ctx.key {
             let change_in_pos = match key {
                 VirtualKeyCode::Left => Point::new(-1, 0),
@@ -21,11 +21,13 @@ impl Player {
             let new_pos = self.pos + change_in_pos;
             if map.can_enter_tile(self.pos) {
                 self.pos = new_pos;
+                camera.on_player_move(new_pos);
             }
         }
     }
 
     pub fn render(&self, ctx: &mut BTerm) {
+        ctx.set_active_console(1);
         ctx.set(self.pos.x, self.pos.y, RED, BLACK, to_cp437('@'));
     }
 }
