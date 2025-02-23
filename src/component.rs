b8e02565a38119b7dd2b1fa6a3f40b67220fe3e7
Une Nyide <une.nyide@gmail.com>
2025-02-23 20:38:08 +0200

    add render and player components to component module

--- src/component.rs
+++ src/component.rs
@@ -1,1 +1,11 @@
 //All available components
+pub use crate::prelude::*;
+
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub struct Player;
