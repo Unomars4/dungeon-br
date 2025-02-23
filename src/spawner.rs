554791489cc32683c6b1c89c1ebefc45cdb965ea
Une Nyide <une.nyide@gmail.com>
2025-02-23 20:46:29 +0200

    add spawn player method to spawner module

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,1 +1,13 @@
 //Module handles spawning entities
+pub use crate::prelude::*;
+
+pub fn spawn_player(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Player,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('@'),
+        },
+    ));
+}
