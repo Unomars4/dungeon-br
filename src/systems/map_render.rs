7067d6ea2ada486ba3c2f02bf47e23b81612946b
Une Nyide <une.nyide@gmail.com>
2025-02-24 13:02:08 +0200

    fix values camera resource

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -6,9 +6,9 @@ pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
     for y in camera.top_y..=camera.bottom_y {
-        for x in camera.left_x..=camera.right_x {
+        for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
-            let offset = Point::new(camera.left_x, camera.bottom_y);
+            let offset = Point::new(camera.left_x, camera.top_y);
             if map.in_bounds(pt) {
                 let idx = map_idx(x, y);
                 let glyph = match map.tiles[idx] {
