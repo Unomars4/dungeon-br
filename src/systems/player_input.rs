356492efc8f31067efadf58fd2a575160c646f53
Une Nyide <une.nyide@gmail.com>
2025-02-24 10:26:47 +0200

    add player_input stubb

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,1 +1,12 @@
 use crate::prelude::*;
+
+#[system]
+#[write_component(Point)]
+#[read_component(Player)]
+fn player_input(
+    &mut ecs: World,
+    #[resource] map: Map,
+    #[resource] key: &Option<VirtualKeyCode>,
+    #[resource] camera: Camera,
+) {
+}
