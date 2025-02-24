ca7bb77e764d55d493b8d0714b08ca8124c3c644
Une Nyide <une.nyide@gmail.com>
2025-02-24 12:00:49 +0200

    add entity render logic to entity render system

--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -1,2 +1,18 @@
 //System renders all entities with Point and Render components
 use crate::prelude::*;
+
+#[system]
+#[read_component(Point)]
+#[read_component(Render)]
+pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
+    let mut draw_batch = DrawBatch::new();
+    draw_batch.target(1);
+    let offset = Point::new(camera.left_x, camera.bottom_y);
+
+    <(&Point, &Render)>::query()
+        .iter(ecs)
+        .for_each(|(pos, render)| {
+            draw_batch.set(*pos - offset, render.color, render.glyph);
+        });
+    draw_batch.submit(5000).expect("Batch error");
+}
