c561b7b97801ce389ba0186232e724d0af7300ce
Une Nyide <une.nyide@gmail.com>
2025-02-24 10:06:33 +0200

    add scheduler build execution to tick function and add ctx key to resources

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,8 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        //TODO:: Execute Systems
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
         //TODO: Render Draw Buffer
     }
 }
