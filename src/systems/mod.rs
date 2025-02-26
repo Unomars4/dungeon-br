2b73f119be85361971ce23087f9714f81fde34db
Une Nyide <une.nyide@gmail.com>
2025-02-26 10:04:52 +0200

    add random movement system to system scheduler and add flush after collision detection system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -10,7 +10,9 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(collision::collisions_system())
+        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(collision::collisions_system())
         .build()
 }
