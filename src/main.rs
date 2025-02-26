a48e5a9a3ccb1404c2deedac9982182a0872da4c
Une Nyide <une.nyide@gmail.com>
2025-02-26 11:04:20 +0200

    add system execution to game tick for input, monster and player systems

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,12 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        self.input_systems
+            .execute(&mut self.ecs, &mut self.resources);
+        self.player_systems
+            .execute(&mut self.ecs, &mut self.resources);
+        self.monster_systems
+            .execute(&mut self.ecs, &mut self.resources);
         render_draw_buffer(ctx).expect("Render error")
     }
 }
