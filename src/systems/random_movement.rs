ad9b1b0c001b1464349150e2ab5981ef81f236c3
Une Nyide <une.nyide@gmail.com>
2025-02-26 10:18:20 +0200

    fix random move logic

--- src/systems/random_movement.rs
+++ src/systems/random_movement.rs
@@ -10,8 +10,8 @@ pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
         let destination = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
             1 => Point::new(1, 0),
-            2 => Point::new(0, 1),
-            _ => Point::new(0, -1),
+            2 => Point::new(0, -1),
+            _ => Point::new(0, 1),
         } + *pos;
 
         if map.can_enter_tile(destination) {
