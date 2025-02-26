caa780f2cf406915ced30f5513d23907f2000d52
Une Nyide <une.nyide@gmail.com>
2025-02-26 10:12:21 +0200

    add moving randomly system correctly

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -13,6 +13,6 @@ pub fn build_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
-        .add_system(collision::collisions_system())
+        .add_system(random_movement::random_move_system())
         .build()
 }
