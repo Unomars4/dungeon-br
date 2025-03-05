316633c4b9ad00dfbd00e14cfe53d17561f3cb8c
Une Nyide <une.nyide@gmail.com>
2025-03-05 19:40:57 +0200

    add orc helper that return hit point, orc name and char type

--- src/spawner.rs
+++ src/spawner.rs
@@ -20,6 +20,10 @@ fn goblin() -> (i32, String, FontCharType) {
     (1, "Goblin".to_string(), to_cp437('g'))
 }
 
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
+}
+
 pub fn spawn_monster(ecs: &mut World, pos: Point, rng: &mut RandomNumberGenerator) {
     ecs.push((
         Enemy,
