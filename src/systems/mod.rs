3c7080a319d0ae90aa6fbd7c091601dfe7a0c56b
Une Nyide <une.nyide@gmail.com>
2025-03-12 12:47:35 +0200

    add chasing system to monster scheduler

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -39,6 +39,7 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_movement::random_move_system())
         .flush()
+        .add_system(chasing::chasing_system())
         .add_system(combat::combat_system())
         .flush()
         .add_system(movement::movement_system())
