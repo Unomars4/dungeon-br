e37762650fbb845eb45790a981ab6bcd1972ed03
Une Nyide <une.nyide@gmail.com>
2025-03-12 20:28:09 +0200

    add spawn_amulet_of_yala function

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,6 +1,19 @@
 //Module handles spawning entities
 pub use crate::prelude::*;
 
+pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Item,
+        AmuletOfYala,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('/'),
+        },
+        Name("Amulet of Yala".to_string()),
+    ));
+}
+
 pub fn spawn_player(ecs: &mut World, pos: Point) {
     ecs.push((
         Player,
