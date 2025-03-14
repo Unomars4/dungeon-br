d5f537dffbf52183308157dbcbc52d2f6d1a0f00
Une Nyide <une.nyide@gmail.com>
2025-03-14 13:55:57 +0200

    refactor entity render system to cater for player field of view hiding entities which are not in the field of view of the player

--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -1,18 +1,25 @@
 //System renders all entities with Point and Render components
-use crate::prelude::*;
+use crate::{component, prelude::*};
 
 #[system]
 #[read_component(Point)]
 #[read_component(Render)]
+#[write_component(FieldOfView)]
+#[read_component(Player)]
 pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
-    draw_batch.target(1);
+    let mut renderables = <(&Point, &Render)>::query();
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
     let offset = Point::new(camera.left_x, camera.top_y);
 
-    <(&Point, &Render)>::query()
+    draw_batch.target(1);
+    renderables
         .iter(ecs)
+        .filter(|(pos, _)| player_fov.visible_tiles.contains(&pos))
         .for_each(|(pos, render)| {
             draw_batch.set(*pos - offset, render.color, render.glyph);
         });
+
     draw_batch.submit(5000).expect("Batch error");
 }
