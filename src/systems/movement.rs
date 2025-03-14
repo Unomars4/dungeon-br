92d0ef8c6a40824eaf793c920a982030204fe9d2
Une Nyide <une.nyide@gmail.com>
2025-03-14 15:22:30 +0200

    add logic to reveal tiles even out of fov as player passes through map

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -8,7 +8,7 @@ use crate::prelude::*;
 pub fn movement(
     entity: &Entity,
     wants_move: &WantsToMove,
-    #[resource] map: &Map,
+    #[resource] map: &mut Map,
     #[resource] camera: &mut Camera,
     ecs: &mut SubWorld,
     command: &mut CommandBuffer,
@@ -19,10 +19,12 @@ pub fn movement(
         if let Ok(entry) = ecs.entry_ref(wants_move.entity) {
             if let Ok(fov) = entry.get_component::<FieldOfView>() {
                 command.add_component(wants_move.entity, fov.clone_dirty());
-            }
-
-            if entry.get_component::<Player>().is_ok() {
-                camera.on_player_move(wants_move.destination);
+                if entry.get_component::<Player>().is_ok() {
+                    camera.on_player_move(wants_move.destination);
+                    fov.visible_tiles
+                        .iter()
+                        .for_each(|pos| map.revealed_tiles[map_idx(pos.x, pos.y)] = true);
+                }
             }
         }
     }
