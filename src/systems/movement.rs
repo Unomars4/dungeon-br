24c806a3015c580014e737126a6e96dfa628f569
Une Nyide <une.nyide@gmail.com>
2025-03-14 14:17:47 +0200

    refactor movement system to make use of field of view component

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -4,6 +4,7 @@ use crate::prelude::*;
 
 #[system(for_each)]
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn movement(
     entity: &Entity,
     wants_move: &WantsToMove,
@@ -12,16 +13,18 @@ pub fn movement(
     ecs: &mut SubWorld,
     command: &mut CommandBuffer,
 ) {
+    let mut fov = <&FieldOfView>::query();
     if map.can_enter_tile(wants_move.destination) {
         command.add_component(wants_move.entity, wants_move.destination);
 
-        if ecs
-            .entry_ref(wants_move.entity)
-            .unwrap()
-            .get_component::<Player>()
-            .is_ok()
-        {
-            camera.on_player_move(wants_move.destination);
+        if let Ok(entry) = ecs.entry_ref(wants_move.entity) {
+            if let Ok(fov) = entry.get_component::<FieldOfView>() {
+                command.add_component(wants_move.entity, fov.clone_dirty());
+            }
+
+            if entry.get_component::<Player>().is_ok() {
+                camera.on_player_move(wants_move.destination);
+            }
         }
     }
 
