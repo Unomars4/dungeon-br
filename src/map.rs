97a40d7ed571323b6274f4360c035ae9e044688a
Une Nyide <une.nyide@gmail.com>
2025-03-14 12:28:39 +0200

    add is_opaque method to BaseMap implementation

--- src/map.rs
+++ src/map.rs
@@ -74,6 +74,10 @@ impl BaseMap for Map {
         exits
     }
 
+    fn is_opaque(&self, idx: usize) -> bool {
+        self.tiles[idx as usize] != TileType::Floor
+    }
+
     fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
         DistanceAlg::Pythagoras.distance2d(self.index_to_point2d(idx1), self.index_to_point2d(idx2))
     }
