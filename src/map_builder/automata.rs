fb2700c15e3f0b195e58fe70cfd219e6fa13379e
Une Nyide <une.nyide@gmail.com>
2025-03-20 12:04:05 +0200

    add constructor to automata architect

--- src/map_builder/automata.rs
+++ src/map_builder/automata.rs
@@ -1,2 +1,18 @@
 use super::MapArchitect;
 use crate::prelude::*;
+
+pub struct AutomataArchitect {}
+
+impl MapArchitect for AutomataArchitect {
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
+        let mut mb = MapBuilder {
+            map: Map::new(),
+            rooms: Vec::new(),
+            player_start: Point::zero(),
+            amulet_start: Point::zero(),
+            monster_spawns: Vec::new(),
+        };
+
+        mb
+    }
+}
