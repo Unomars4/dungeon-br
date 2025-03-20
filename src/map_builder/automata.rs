14b820ef9fbc6332dbc637bb21d5ce84bce2fb03
Une Nyide <une.nyide@gmail.com>
2025-03-20 12:41:25 +0200

    add find_start method to automata architect impl

--- src/map_builder/automata.rs
+++ src/map_builder/automata.rs
@@ -56,4 +56,23 @@ impl AutomataArchitect {
         }
         map.tiles = new_tiles;
     }
+
+    fn find_start(&self, map: &Map) -> Point {
+        let center = Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+        let closest_point = map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(_, t)| **t == TileType::Floor)
+            .map(|(idx, _)| {
+                (
+                    idx,
+                    DistanceAlg::Pythagoras.distance2d(center, map.index_to_point2d(idx)),
+                )
+            })
+            .min_by(|(_, distance), (_, distance2d)| distance.partial_cmp(&distance2d).unwrap())
+            .map(|(idx, _)| idx)
+            .unwrap();
+        map.index_to_point2d(closest_point)
+    }
 }
