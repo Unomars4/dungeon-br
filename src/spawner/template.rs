13803f4147a114ef325369e0cdf8ff8213cb2bb7
Une Nyide <une.nyide@gmail.com>
2025-03-25 16:04:48 +0200

    add template struct to template spawner module

--- src/spawner/template.rs
+++ src/spawner/template.rs
@@ -1,2 +1,17 @@
 use crate::prelude::*;
+use legion::systems::CommandBuffer;
+use ron::de::from_reader;
 use serde::Deserialize;
+use std::collections::HashSet;
+use std::fs::File;
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Template {
+    pub entity_type: EntityType,
+    pub levels: HashSet<usize>,
+    pub frequency: i32,
+    pub name: String,
+    pub glyph: char,
+    pub provides: Option<Vec<(String, i32)>>,
+    pub hp: Option<i32>,
+}
