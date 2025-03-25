4a8a1a0c0ce15f2b4bd10372a87eefc3df2e4db8
Une Nyide <une.nyide@gmail.com>
2025-03-25 14:04:36 +0200

    update end turn system to handle when player is changing map levels

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -5,7 +5,7 @@ use crate::prelude::*;
 #[read_component(Health)]
 #[read_component(AmuletOfYala)]
 #[read_component(Point)]
-pub fn end_turn(#[resource] turn_state: &mut TurnState, ecs: &SubWorld) {
+pub fn end_turn(#[resource] turn_state: &mut TurnState, ecs: &SubWorld, #[resource] map: &Map) {
     let mut player_hp = <(&Health, &Point)>::query().filter(component::<Player>());
     let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
     let amulet_default = Point::new(-1, -1);
@@ -19,12 +19,18 @@ pub fn end_turn(#[resource] turn_state: &mut TurnState, ecs: &SubWorld) {
     };
 
     player_hp.iter(ecs).for_each(|(hp, pos)| {
+        let idx = map.point2d_to_index(*pos);
+
         if hp.current < 1 {
             new_state = TurnState::GameOver;
         }
 
         if pos == amulet_pos {
             new_state = TurnState::Victory;
+        }
+
+        if map.tiles[idx] == TileType::Exit {
+            new_state = TurnState::NextLevel;
         }
     });
 
