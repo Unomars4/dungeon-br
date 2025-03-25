3b7c29ac0156a8c4ea3fde346f9d16a292c13a58
Une Nyide <une.nyide@gmail.com>
2025-03-25 09:24:34 +0200

    add logic to hud system to show player his inventory of carried items

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -27,4 +27,27 @@ pub fn hud(ecs: &SubWorld) {
         ColorPair::new(WHITE, RED),
     );
     draw_batch.submit(10000).expect("Batch error");
+
+    let player = <(Entity, &Player)>::query()
+        .iter(ecs)
+        .find_map(|(entity, _player)| Some(*entity))
+        .unwrap();
+
+    let mut items_query = <(&Name, &Item, &Carried)>::query();
+    let mut y = 3;
+    items_query
+        .iter(ecs)
+        .filter(|(_, _, carried)| carried.0 == player)
+        .for_each(|(name, _, _)| {
+            draw_batch.print(Point::new(3, y), format!("{}: {}", y - 2, &name.0));
+            y += 1;
+        });
+
+    if y > 3 {
+        draw_batch.print_color(
+            Point::new(3, 2),
+            "Items carried",
+            ColorPair::new(BLACK, YELLOW),
+        );
+    }
 }
