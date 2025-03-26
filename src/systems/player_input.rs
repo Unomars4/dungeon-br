d592e04095171e4a32f08fa132b45c504260d7b2
Une Nyide <une.nyide@gmail.com>
2025-03-26 12:15:55 +0200

    add logic to player input that only allows to swords to affect damage total in combat

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -39,6 +39,17 @@ pub fn player_input(
                     .for_each(|(entity, _item, _item_pos)| {
                         commands.remove_component::<Point>(*entity);
                         commands.add_component(*entity, Carried(player));
+
+                        if let Ok(e) = ecs.entry_ref(*entity) {
+                            if e.get_component::<Weapon>().is_ok() {
+                                <(Entity, &Carried, &Weapon)>::query()
+                                    .iter(ecs)
+                                    .filter(|(_, c, _)| c.0 == *entity)
+                                    .for_each(|(e, _, _)| {
+                                        commands.remove(*e);
+                                    });
+                            }
+                        }
                     });
                 Point::new(0, 0)
             }
